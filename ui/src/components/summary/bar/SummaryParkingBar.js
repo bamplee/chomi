90d6a30d34bd3686e55a9cb397cd8ebe77267958
ih.lee <bamplee@gmail.com>
2019-06-01 14:37:20 +0900

    https://github.com/bamplee/chomi/issues/10 서머리 탭 주차장 정보 디스크립션 스타일 변경

--- ui/src/components/summary/bar/SummaryParkingBar.js
+++ ui/src/components/summary/bar/SummaryParkingBar.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Card, Typography} from 'antd';
+import {Card, List} from 'antd';
 import {inject, observer} from 'mobx-react';
 
 @inject('searchStore')
@@ -15,19 +15,21 @@ class SummaryParkingBar extends Component {
                      alt="logo"
                      src={'http://101.101.161.132/api/v1/maps/image?x=' + searchStore.parking.LNG + '&y=' + searchStore.parking.LAT}
                 />
-                <Typography.Text>{searchStore.parking.PARKING_NAME}</Typography.Text>
-                <p>{searchStore.parking.address}</p>
-                <div>
-                    <div>
-                        {searchStore.parking.ADDR}
-                    </div>
-                    <div style={{display: 'flex', justifyContent: 'space-between', paddingTop: 8}}>
-                        {searchStore.parking.ADD_RATES}원/{searchStore.parking.ADD_TIME_RATE}분
-                    </div>
-                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                        주말 {searchStore.parking.SATURDAY_PAY_NM}, 공휴일 {searchStore.parking.HOLIDAY_PAY_NM}
-                    </div>
-                </div>
+                <List.Item.Meta
+                    title={searchStore.parking.PARKING_NAME}
+                    description={
+                        <div>
+                            <div>
+                                {searchStore.parking.ADDR}
+                            </div>
+                            <div style={{display: 'flex', justifyContent: 'space-between', paddingTop: 8}}>
+                                {searchStore.parking.ADD_RATES}원/{searchStore.parking.ADD_TIME_RATE}분
+                            </div>
+                            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                                주말 {searchStore.parking.SATURDAY_PAY_NM}, 공휴일 {searchStore.parking.HOLIDAY_PAY_NM}
+                            </div>
+                        </div>}
+                />
             </Card>
         )
     }
